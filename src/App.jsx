1e358516e0f7bee765fef90c5aee8f7f47731c53
Doug <douggyharmon@gmail.com>
2024-03-09 16:01:27 -0500

    basic nav + page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getAllData } from './util/index';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 const URL = 'http://localhost:8000/api/v1/';
 
@@ -20,9 +21,52 @@ function App() {
 
   }, []);
 
+  const LandingPage = () => {
+    return (
+      <>
+      <h1>Pet Project</h1>
+        <Link to='/mission'>
+          Mission
+        </Link>
+        <Link to='/adopt'>
+          Adopt
+        </Link>
+        <Link to='/faq'>
+          FAQ
+        </Link>
+        {message ? <h3>back end active</h3> : <h3>back end inactive</h3>}
+      </>
+    )
+  }
+
   return (
     <>
-      <h1>{message}</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/faq' element={<>
+            <h1>Frequently Asked Questions</h1>
+            <Link to='/'>
+              <button>Back</button>
+            </Link>
+          </>}
+          />
+          <Route path='/mission' element={<>
+            <h1>Our Mission</h1>
+            <Link to='/'>
+              <button>Back</button>
+            </Link>
+          </>}
+          />
+          <Route path='/adopt' element={<>
+            <h1>Find a Pet</h1>
+            <Link to='/'>
+              <button>Back</button>
+            </Link>
+          </>}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 
