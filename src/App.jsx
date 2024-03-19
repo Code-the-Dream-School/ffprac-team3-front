3a3d991681f9839ad073e319922bae582bdd0430
Keri West <keriwest88@gmail.com>
2024-03-19 18:10:38 -0400

    sprint 1.2: created a navbar component with a drop menu for the profile account routes that will need to be connected. Started utilizing Material UI library for design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { getAllData } from './util/index';
+import React, { useState, useEffect } from "react";
+import { getAllData } from "./util/index";
+import { Navbar } from "./components/Navbar/Navbar";
 
-const URL = 'http://localhost:8000/api/v1/';
+const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-  
-  const [message, setMessage] = useState(''); 
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-
     (async () => {
-      const myData = await getAllData(URL)
+      const myData = await getAllData(URL);
       setMessage(myData.data);
     })();
-      
-    return () => {
-      console.log('unmounting');
-    }
 
+    return () => {
+      console.log("unmounting");
+    };
   }, []);
 
   return (
-    <>
-      <h1>{message}</h1>
-    </>
-  );
+    <div>
+      <Navbar />
 
+      {/* <h1>{message}</h1> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+  Menu,
+  MenuItem,
+  Link,
+} from "@mui/material";
+import PetsIcon from "@mui/icons-material/Pets";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleAccountClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar className={styles.Toolbar}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
+            <PetsIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            PetPals App
+          </Typography>
+
+          <Stack direction="row" spacing={2}>
+            <Button color="inherit">
+              <FavoriteIcon />
+            </Button>
+
+            <Button
+              color="inherit"
+              id="account-button"
+              onClick={handleAccountClick}
+              aria-controls={open ? "account-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              endIcon={<KeyboardArrowDownIcon />}
+            >
+              <AccountCircleIcon />
+            </Button>
+          </Stack>
+
+          <Menu
+            id="account-menu"
+            anchorEl={anchorEl}
+            open={open}
+            MenuListProps={{
+              "aria-labelledby": "account-button",
+            }}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link href="#" color="#0E2728" underline="none">
+                Login
+              </Link>
+            </MenuItem>
+
+            {/* These options will be hidden when logged out */}
+            <MenuItem onClick={handleClose}>
+              <Link href="#" color="#0E2728" underline="none">
+                About Me
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="#" color="#0E2728" underline="none">
+                Adopter Profile
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="#" color="#0E2728" underline="none">
+                Account Settings
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="#" color="#0E2728" underline="none">
+                Logout
+              </Link>
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
