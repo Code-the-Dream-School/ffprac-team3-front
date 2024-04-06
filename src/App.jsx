dddea05ceb5143a88ebd7637f42d10366078d653
Doug <douggyharmon@gmail.com>
2024-04-05 20:59:18 -0400

    add temporary button linking to Resources page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { getAllData } from "./util/index";
 import { Navbar } from "./components/Navbar";
-import { HeroBanner } from "./components/HeroBanner/HeroBanner";
+import { HeroBanner } from "./components/HeroBanner";
 import { PetSliderCarousel } from "./components/PetSliderCarousel";
+import { Button, Box } from "@mui/material";
 
 const URL = "http://localhost:8000/api/v1/";
 
@@ -24,6 +25,29 @@ function App() {
     <>
       <Navbar />
       <HeroBanner />
+
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+      >
+        <Button
+          href="/resources"
+          variant="contained"
+          sx={{
+            mt: 2,
+            textTransform:"none",
+            fontStyle:"italic",
+            color: "#F7F4F0",
+            backgroundColor: "#EE633E",
+            "&:hover": {
+              backgroundColor: "#F8AF3F",
+            }
+          }}
+        >
+          Temporary shortcut to Resources page
+        </Button>
+      </Box>
+      
       <PetSliderCarousel />
 
       {/* <h1>{message}</h1> */}
