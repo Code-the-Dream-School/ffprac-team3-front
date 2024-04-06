dd535069fb236b83ba86c20cf7914f123979ee75
Doug <douggyharmon@gmail.com>
2024-04-05 22:28:44 -0400

    add ResourcesPage component + route to get there

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ResourcesPage from './pages/ResourcesPage.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/resources' element={<ResourcesPage/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/ResourcesPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+const ResourcesPage = () => {
+    return (
+        <>
+        <Box>
+        <Typography variant="h2">This is our Resources page</Typography>
+        </Box>
+        </>
+    )
+}
+
+export default ResourcesPage
