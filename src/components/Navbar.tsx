c0e426562712136447f182a33d77589c1a84ae2f
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-04-26 16:50:27 -0700

    feat: updated Navbar.tsx to conditionally display login if no user is logged in and to show additional user options once logged-in.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   AppBar,
@@ -9,32 +9,31 @@ import {
   MenuItem,
   Link,
   MenuList,
-} from "@mui/material";
-import { PetLogoIcon } from "../img/icons/PetLogoIcon";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import Divider from "@mui/material/Divider";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { loginUser } from "../util/index";
-import { useNavigate } from "react-router-dom";
+} from '@mui/material';
+import { PetLogoIcon } from '../img/icons/PetLogoIcon';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Divider from '@mui/material/Divider';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { useNavigate } from 'react-router-dom';
 
 export const Navbar: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track login status
-  const [userName, setUserName] = useState(""); // State to store the user's name
+  const [userName, setUserName] = useState(''); // State to store the user's name
   const navigate = useNavigate();
 
   useEffect(() => {
     // Check if the user is logged in by checking if jwtToken exists in localStorage
-    const jwtToken = localStorage.getItem("jwtToken");
-    const storedUserName = localStorage.getItem("userName");
+    const jwtToken = localStorage.getItem('jwtToken');
+    const storedUserName = localStorage.getItem('firstName');
     if (jwtToken) {
       setIsLoggedIn(true);
-      setUserName(storedUserName || "");
+      setUserName(storedUserName || '');
     } else {
       setIsLoggedIn(false);
-      setUserName("");
+      setUserName('');
     }
   }, []);
 
@@ -45,14 +44,6 @@ export const Navbar: React.FC = () => {
 
   const handleClose = async () => {
     setAnchorEl(null);
-
-    try {
-      // Call the loginUser function to check if the user is logged in
-      const response = await loginUser(loginUser);
-      setIsLoggedIn(true);
-    } catch (error) {
-      setIsLoggedIn(false);
-    }
   };
 
   return (
@@ -60,9 +51,9 @@ export const Navbar: React.FC = () => {
       <AppBar
         position="fixed"
         sx={{
-          backgroundColor: "#506c60",
-          borderColor: "none",
-          color: "#f7f4f0",
+          backgroundColor: '#506c60',
+          borderColor: 'none',
+          color: '#f7f4f0',
         }}
       >
         <Toolbar>
@@ -70,15 +61,15 @@ export const Navbar: React.FC = () => {
             aria-label="logo"
             href="/"
             sx={{
-              height: "1rem",
-              "&:hover": {
-                backgroundColor: "none",
+              height: '1rem',
+              '&:hover': {
+                backgroundColor: 'none',
               },
             }}
           >
             <PetLogoIcon
               sx={{
-                fontSize: "7rem",
+                fontSize: '7rem',
               }}
             />
           </IconButton>
@@ -89,24 +80,24 @@ export const Navbar: React.FC = () => {
             justifyContent="flex-end"
             sx={{ flexGrow: 1 }}
           >
-            {/* {isLoggedIn && ( // Render only if logged in */}
-
             <>
-              <IconButton
-                id="favorites"
-                color="inherit"
-                onClick={(event) => {
-                  navigate("/search?favorites=true");
-                }}
-                sx={{
-                  "&:hover": {
-                    color: "#F8AF3F",
-                    backgroundColor: "transparent",
-                  },
-                }}
-              >
-                <FavoriteIcon />
-              </IconButton>
+              {isLoggedIn && (
+                <IconButton
+                  id="favorites"
+                  color="inherit"
+                  onClick={(event) => {
+                    navigate('/search?favorites=true');
+                  }}
+                  sx={{
+                    '&:hover': {
+                      color: '#F8AF3F',
+                      backgroundColor: 'transparent',
+                    },
+                  }}
+                >
+                  <FavoriteIcon />
+                </IconButton>
+              )}
             </>
             <Divider
               color="#F8AF3F"
@@ -115,19 +106,17 @@ export const Navbar: React.FC = () => {
               flexItem
             />
 
-            {/* )} */}
-
             <IconButton
               color="inherit"
               id="account-button"
               onClick={handleAccountClick}
-              aria-controls={open ? "account-menu" : undefined}
+              aria-controls={open ? 'account-menu' : undefined}
               aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
+              aria-expanded={open ? 'true' : undefined}
               sx={{
-                "&:hover": {
-                  color: "#F8AF3F",
-                  backgroundColor: "transparent",
+                '&:hover': {
+                  color: '#F8AF3F',
+                  backgroundColor: 'transparent',
                 },
               }}
             >
@@ -135,7 +124,7 @@ export const Navbar: React.FC = () => {
               <KeyboardArrowDownIcon />
 
               {/* {/* Display user's name if logged in */}
-              {/* {isLoggedIn && <span>{userName}</span>} */}
+              {isLoggedIn && <span>{userName}</span>}
             </IconButton>
           </Stack>
 
@@ -144,53 +133,59 @@ export const Navbar: React.FC = () => {
             anchorEl={anchorEl}
             open={open}
             MenuListProps={{
-              "aria-labelledby": "account-button",
+              'aria-labelledby': 'account-button',
             }}
             onClose={handleClose}
             anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "right",
+              vertical: 'bottom',
+              horizontal: 'right',
             }}
             transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
+              vertical: 'top',
+              horizontal: 'right',
             }}
           >
             <MenuList dense>
-              {/* {!isLoggedIn && ( // Render only if not logged in */}
-              <MenuItem onClick={handleClose}>
-                <Link href="/login" underline="none">
-                  Login
-                </Link>
-              </MenuItem>
-              {/* )} */}
+              {/* Render only if not logged in */}
+              {!isLoggedIn && (
+                <MenuItem onClick={handleClose}>
+                  <Link href="/login" underline="none">
+                    Login
+                  </Link>
+                </MenuItem>
+              )}
 
               {/* These options will be hidden when logged out */}
-              {/* {isLoggedIn && ( // Render only if logged in */}
 
-              <MenuItem onClick={handleClose}>
-                <Link href="/profile" color="#0E2728" underline="none">
-                  Profile
-                </Link>
-              </MenuItem>
+              {isLoggedIn && (
+                <MenuItem onClick={handleClose}>
+                  <Link href="/profile" color="#0E2728" underline="none">
+                    Profile
+                  </Link>
+                </MenuItem>
+              )}
 
-              <MenuItem onClick={handleClose}>
-                <Link
-                  href="/profile/#settings"
-                  color="#0E2728"
-                  underline="none"
-                >
-                  Account Settings
-                </Link>
-              </MenuItem>
-              <Divider />
-              <MenuItem onClick={handleClose}>
-                <Link href="/logout" color="#0E2728" underline="none">
-                  Logout
-                </Link>
-              </MenuItem>
+              {isLoggedIn && (
+                <MenuItem onClick={handleClose}>
+                  <Link
+                    href="/profile/#settings"
+                    color="#0E2728"
+                    underline="none"
+                  >
+                    Account Settings
+                  </Link>
+                </MenuItem>
+              )}
 
-              {/* )} */}
+              {isLoggedIn && <Divider />}
+
+              {isLoggedIn && (
+                <MenuItem onClick={handleClose}>
+                  <Link href="/logout" color="#0E2728" underline="none">
+                    Logout
+                  </Link>
+                </MenuItem>
+              )}
             </MenuList>
           </Menu>
         </Toolbar>
