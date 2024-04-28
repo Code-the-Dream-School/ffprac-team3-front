8dcecf931ddd4fb8807a4270354e8ecfaa6f71de
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-04-28 09:58:22 -0700

    Merge conflicts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { HeroBanner } from './components/HomePageComponents/HeroBanner';
@@ -13,25 +11,7 @@ import { PetProfile } from './components/PetProfileComponent/PetProfile';
 import { ContactForm } from './components/ContactForm';
 import { ResourcesBanner } from './components/HomePageComponents/ResourcesBanner.tsx';
 import { UserProfile } from './components/UserProfileComponents/UserProfile';
-import { ProfileSettings } from './components/UserProfileComponents/ProfileSettings';
-=======
-import React, { useState, useEffect } from "react";
-import { getData } from "./util/index";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { HeroBanner } from "./components/HomePageComponents/HeroBanner";
-import { PetSliderCarousel } from "./components/HomePageComponents/PetSliderCarousel";
-import { SearchPets } from "./components/SearchComponents/SearchPets";
-import ResourcesPage from "./pages/ResourcesPage.tsx";
-import { LoginForm } from "./components/LoginForm/LoginForm";
-import { Footer } from "./components/footer";
-import { SignUpForm } from "./components/SignUpForm/SignUpForm";
-import { PetProfile } from "./components/PetProfileComponent/PetProfile";
-import { ContactForm } from "./components/ContactForm";
-import { ResourcesBanner } from "./components/HomePageComponents/ResourcesBanner.tsx";
-
-const URL = "http://localhost:8000/api/v1/";
->>>>>>> main
+import { PetProfile } from './components/PetProfileComponent/PetProfile';
 
 function App() {
   return (
@@ -64,9 +44,7 @@ function App() {
         />
 
         <Route
-
           path="/pet-profile/:_id/:type/:name"
-
           element={
             <>
               <Navbar />
