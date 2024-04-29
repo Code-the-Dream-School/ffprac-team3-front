be5cef3b4d0a7ddf59e3e29453f384aa60c31f8e
Doug <douggyharmon@gmail.com>
2024-04-29 14:53:22 -0400

    adds a reusable ResLinkCard component for ResLinkCards

--- src/components/ResLinkCards.tsx
+++ src/components/ResLinkCards.tsx
@@ -1,6 +1,36 @@
 import React from "react"
 import { Typography, Box, Card, CardContent, CardMedia, Link } from "@mui/material";
 
+const ResLinkCard = ({ link, image, title, description }) => {
+    return (
+        <Link
+            underline="none"
+            href={link}
+            target="_blank"
+            rel="noopener"
+        >
+            <Card square variant="outlined">
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={image}
+                />
+                <CardContent sx={{ width: 200, height: 170 }}>
+                    <Typography
+                        color="primary"
+                        sx={{ textDecoration: "underline lightblue" }}
+                    >
+                        {title}
+                    </Typography>
+
+                    <Typography>
+                        {description}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Link>
+    )
+}
+
 const ResLinkCards = () => {
     return (
         <Box
@@ -11,77 +41,33 @@ const ResLinkCards = () => {
             justifyContent="space-evenly"
             alignItems="center"
         >
-            <Link
-                underline="none"
-                href="https://www.aspca.org/pet-care/dog-care/general-dog-care"
-                target="_blank"
-                rel="noopener"
-            >
-                <Card square variant="outlined">
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="https://www.aspca.org/sites/default/files/dog-care_general-dog-care_main-image.jpg"
-                    />
-                    <CardContent sx={{ width: 200, height: 170 }}>
-                        <Typography color="primary" sx={{ textDecoration: "underline lightblue" }}>General Dog Care</Typography>
-                        <Typography>A beginner's guide to preparing and caring for a dog.</Typography>
-                    </CardContent>
-                </Card>
-            </Link>
+            <ResLinkCard
+                link={"https://www.aspca.org/pet-care/dog-care/general-dog-care"}
+                image={"https://www.aspca.org/sites/default/files/dog-care_general-dog-care_main-image.jpg"}
+                title={"General Dog Care"}
+                description={"A beginner's guide to preparing and caring for a dog."}
+            />
 
-            <Link
-                underline="none"
-                href="https://www.aspca.org/pet-care/cat-care/general-cat-care"
-                target="_blank"
-                rel="noopener"
-            >
-                <Card square variant="outlined">
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="https://www.aspca.org/sites/default/files/cat-care_general-cat-care_body1-right.jpg"
-                    />
-                    <CardContent sx={{ width: 200, height: 170 }}>
-                        <Typography color="primary" sx={{ textDecoration: "underline lightblue" }}>General Cat Care</Typography>
-                        <Typography>A beginner's guide to preparing and caring for a cat.</Typography>
-                    </CardContent>
-                </Card>
-            </Link>
+            <ResLinkCard
+                link={"https://www.aspca.org/pet-care/cat-care/general-cat-care"}
+                image={"https://www.aspca.org/sites/default/files/cat-care_general-cat-care_body1-right.jpg"}
+                title={"General Cat Care"}
+                description={"A beginner's guide to preparing and caring for a cat."}
+            />
 
-            <Link
-                underline="none"
-                href="https://www.petfinder.com/other-pets/small-and-furry-pets/facts-small-mammal-pets/"
-                target="_blank"
-                rel="noopener"
-            >
-                <Card square variant="outlined">
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="https://www.petfinder.com/static/e801888dd7dc3c3a735547036e840078/5a4d9/hamster-facts-about-small-mammals-as-pets-thinkstock-155233157-253x168_0.webp"
-                    />
-                    <CardContent sx={{ width: 200, height: 170 }}>
-                        <Typography color="primary" sx={{ textDecoration: "underline lightblue" }}>Facts About Small Mammals as Pets</Typography>
-                        <Typography>A guide for those smaller furry friends, complete with a handy characteristics chart.</Typography>
-                    </CardContent>
-                </Card>
-            </Link>
+            <ResLinkCard
+                link={"https://www.petfinder.com/other-pets/small-and-furry-pets/facts-small-mammal-pets/"}
+                image={"https://www.petfinder.com/static/e801888dd7dc3c3a735547036e840078/5a4d9/hamster-facts-about-small-mammals-as-pets-thinkstock-155233157-253x168_0.webp"}
+                title={"Facts About Small Mammals as Pets"}
+                description={"A guide for those smaller furry friends, complete with a handy characteristics chart."}
+            />
 
-            <Link
-                underline="none"
-                href="https://vetericyn.com/blog/reptile-care-for-beginners-a-helpful-guide/"
-                target="_blank"
-                rel="noopener"
-            >
-                <Card square variant="outlined">
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="https://vetericyn.com/Vetericyn/wp-content/uploads/2020/10/Small-pet-tortoise-eating-lettuce-in-a-pet-shop-tank.jpg"
-                    />
-                    <CardContent sx={{ width: 200, height: 170 }}>
-                        <Typography color="primary" sx={{ textDecoration: "underline lightblue" }}>Reptile Care for Beginners: A Helpful Guide</Typography>
-                        <Typography>A comprehensive yet approachable beginner's guide to caring for your reptile friend(s).</Typography>
-                    </CardContent>
-                </Card>
-            </Link>
+            <ResLinkCard
+                link={"https://vetericyn.com/blog/reptile-care-for-beginners-a-helpful-guide/"}
+                image={"https://vetericyn.com/Vetericyn/wp-content/uploads/2020/10/Small-pet-tortoise-eating-lettuce-in-a-pet-shop-tank.jpg"}
+                title={"Reptile Care for Beginners: A Helpful Guide"}
+                description={"A comprehensive yet approachable beginner's guide to caring for your reptile friend(s)."}
+            />
         </Box>
     )
 }
